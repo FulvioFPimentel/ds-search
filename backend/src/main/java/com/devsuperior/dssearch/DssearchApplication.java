3f595228267d59562e6799363e32e652b7426b87
FulvioFPimentel <fulviofp@hotmail.com>
2022-02-05 23:57:38 -0300

    Project ds-search created

---
+++ backend/src/main/java/com/devsuperior/dssearch/DssearchApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dssearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssearchApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dssearch/DssearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dssearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

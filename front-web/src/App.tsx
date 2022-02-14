0352ca52bfbf20752220fca373dfe92170f8c46b
FulvioFPimentel <fulviofp@hotmail.com>
2022-02-14 17:02:13 -0300

    Created home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
